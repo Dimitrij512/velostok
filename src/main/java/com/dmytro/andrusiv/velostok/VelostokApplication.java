c9a4cc246e90474a23e1d2dd396c01b417bac01c
Dmytro Andrusiv <hfl-if@ukr.net>
2018-04-18 18:35:34 +0300

    first commit

---
+++ src/main/java/com/dmytro/andrusiv/velostok/VelostokApplication.java
@@ -0,0 +1,12 @@
+package com.dmytro.andrusiv.velostok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelostokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelostokApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dmytro/andrusiv/velostok/VelostokApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dmytro.andrusiv.velostok;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VelostokApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
