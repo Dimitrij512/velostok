4b964794dae45503878e3c8c582f6f9a53a2398f
Dimitrij512 <dmytro.andrusiv@gmail.com>
2018-06-02 00:11:13 +0300

    added request maping for admin

--- src/main/java/com/dmytro/andrusiv/velostok/controllers/AdminController.java
+++ src/main/java/com/dmytro/andrusiv/velostok/controllers/AdminController.java
@@ -5,15 +5,17 @@ import com.dmytro.andrusiv.velostok.services.api.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/admin")
 public class AdminController {
 
     @Autowired
     CategoryService cotegoryService;
 
-    @PostMapping("/admin/createCategory")
+    @PostMapping("/createCategory")
     Category createCategory(@RequestBody Category category){
         return  cotegoryService.createCategory(category);
     }
