bc44f7990c9f1db752bd3fa7fc9994ffbabc94e9
Dimitrij512 <dmytro.andrusiv@gmail.com>
2018-06-12 01:35:28 +0300

    test commit for mongodb online 2

--- src/main/java/com/dmytro/andrusiv/velostok/models/Category.java
+++ src/main/java/com/dmytro/andrusiv/velostok/models/Category.java
@@ -2,10 +2,12 @@ package com.dmytro.andrusiv.velostok.models;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Data
+@Document
 public class Category {
     @Id
     String id;
