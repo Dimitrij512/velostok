140b1c6629e6b8041fcac00a9059246e0e1f20df
Dimitrij512 <dmytro.andrusiv@gmail.com>
2018-07-06 20:55:21 +0300

    add controller for find product by name of category

--- src/main/java/com/dmytro/andrusiv/velostok/controllers/ClientController.java
+++ src/main/java/com/dmytro/andrusiv/velostok/controllers/ClientController.java
@@ -35,6 +35,11 @@ public class ClientController {
         return productService.findOneById(productId).get();
     }
 
+    @GetMapping("/products/category/{categoryName}")
+    List<Product> findAllByCategory(@PathVariable String categoryName){
+        return productService.findAllByCategory(categoryName);
+    }
+
     @GetMapping("/products/{name}")
     List<Product> findAllProductsByCategory(@PathVariable String name){
         return productService.findAllByCategory(name);
