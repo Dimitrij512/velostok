51bc8e886f57e26995bf17d03d19d451b5396662
Dimitrij512 <dmytro.andrusiv@gmail.com>
2018-07-14 23:13:38 +0300

    removed redundant comments

--- src/main/java/com/dmytro/andrusiv/velostok/repositories/SubCategoryRepository.java
+++ src/main/java/com/dmytro/andrusiv/velostok/repositories/SubCategoryRepository.java
@@ -1,11 +1,9 @@
 package com.dmytro.andrusiv.velostok.repositories;
 
-import com.dmytro.andrusiv.velostok.models.Product;
 import com.dmytro.andrusiv.velostok.models.SubCategory;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface SubCategoryRepository extends MongoRepository<SubCategory, String> {
--- src/main/java/com/dmytro/andrusiv/velostok/repositories/SuperCategoryRepository.java
+++ src/main/java/com/dmytro/andrusiv/velostok/repositories/SuperCategoryRepository.java
@@ -1,11 +1,8 @@
 package com.dmytro.andrusiv.velostok.repositories;
 
-import com.dmytro.andrusiv.velostok.models.Category;
 import com.dmytro.andrusiv.velostok.models.SuperCategory;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface SuperCategoryRepository extends MongoRepository<SuperCategory, String> {
