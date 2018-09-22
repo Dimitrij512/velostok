c9680f423f03dba9e79ac78f1c613aff2bd0792a
Dimitrij512 <dmytro.andrusiv@gmail.com>
2018-09-22 18:47:52 +0300

    added simple OAUTH

--- src/main/java/com/dmytro/andrusiv/velostok/VelostokApplication.java
+++ src/main/java/com/dmytro/andrusiv/velostok/VelostokApplication.java
@@ -2,11 +2,37 @@ package com.dmytro.andrusiv.velostok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @SpringBootApplication
-public class VelostokApplication {
+@EnableOAuth2Sso
+public class VelostokApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VelostokApplication.class, args);
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.csrf().disable()
+				.antMatcher("/**")
+				.authorizeRequests()
+				.antMatchers("/", "/login**", "/webjars/**")
+				.permitAll()
+				.anyRequest()
+				.authenticated()
+				.and().logout().logoutSuccessUrl("/").permitAll();
+	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
 }
