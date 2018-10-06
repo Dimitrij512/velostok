1f5b5e28f7f1ee242ce2e92da78f1544b2d8a125
Dimitrij512 <dmytro.andrusiv@gmail.com>
2018-10-06 13:37:51 +0300

    added model for user

---
+++ src/main/java/com/dmytro/andrusiv/velostok/models/User.java
@@ -0,0 +1,19 @@
+package com.dmytro.andrusiv.velostok.models;
+
+import com.dmytro.andrusiv.velostok.enums.ApplRole;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+    private ApplRole role;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+}
